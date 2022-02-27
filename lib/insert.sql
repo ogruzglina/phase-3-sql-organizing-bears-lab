88bc77baa7848ecfb046386873399cc68f0e08bd
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-27 17:52:08 -0500

    commit tree 7e7005b2aa3b0735c0ea6752bc505759c17b44e8

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES ('Mr. Chocolate', 23, 'M', 'blue', 'yyy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES ('Rowdy', 87, 'M', 'black', 'nice', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES ('Tabitha', 12, 'F', 'pink', 'kind', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES ('Sergeant Brown', 43, 'M', 'olive', 'rigorous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES ('Melissa', 31, 'F', 'aquamarine', 'patient', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES ('Grinch', 101, 'M', 'brown', 'arrogant', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES ('Wendy', 43, 'F', 'green', 'rude', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES (NULL, 0, 'F', 'white', NULL, 1);
